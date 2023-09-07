a5c4f1986c87596c8a9efb5b929be89ddb0849ff
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-07 07:04:27 -0700

    first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point of the code. It is the start of the code
+ * This code is meant the give the output Hello world.
+ *
+ * Return: the code will always return 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
