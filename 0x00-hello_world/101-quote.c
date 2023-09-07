b79374bd52db7633452eff2155d4759f91fb551b
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-07 16:02:09 -0700

    c programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is the point where the code begins
+ *
+ * Return: if the code returns (1) then it is successful
+ */
+int main(void)
+{
+	fputs ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
