ba143bd60a5a665cb145a933f990310258e0d406
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-07 07:14:39 -0700

    my second program in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the starting of the program
+ *
+ * Return: when (0) is returned, it is a success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
