b429130da0d5c051221275f56341ed752956c12a
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-08 15:27:00 -0700

    unmbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: (0) means success
+ */
+int main(void)
+{
+int m;
+	for(m = 0; m < 10; m++)
+	{
+		putchar('0' + m);
+	}
+	putchar('\n');
+	return (0);
+}
