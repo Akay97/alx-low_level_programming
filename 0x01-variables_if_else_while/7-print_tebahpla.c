529d7bf04ff92b0745e5e91e4c596593bb39711a
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-08 15:49:32 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the code entry
+ *
+ * Return: (0) indicates success
+ */
+int main(void)
+{
+char m;
+	for (m = 'z'; m >= 'a'; m--)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
