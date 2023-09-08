466b7034283bd69a1a99f570d7b233855d41b9d4
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-08 09:02:38 -0700

    alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a defined function
+ *
+ * Return: returning (0) is success
+ */
+int main(void)
+{
+char alphab;
+alphab = 'a';
+	while
+		(alphab <= 'z') {
+			putchar(alphab);
+			alphab++;
+		}
+	putchar('\n');
+	return (0);
+}
