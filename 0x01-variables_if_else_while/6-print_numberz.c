2f75faf6352f3e0cf3388ffd94c82a286630bc94
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-08 15:42:12 -0700

    unmberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: (0) is success
+ */
+int main(void)
+{
+int _;
+	for (_ = 0; _ < 10; _++)
+	{
+		putchar('0' + _);
+	}
+	putchar ('\n');
+	return (0);
+}
