ce429ad800dcfcb5f52f4475a55e0fbbc77d71b0
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-12 15:22:57 -0700

    compute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - the function name
+ * @num: the value to be computed
+ * Return: (0) at success
+ */
+int _abs(int num)
+{
+
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
