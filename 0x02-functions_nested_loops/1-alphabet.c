abd28d7dfeba526b1b8b9b452e8f902c6bbffc45
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-12 07:41:40 -0700

    loops and functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the functin of the code
+ *
+ * Return: (0) is success
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+		_putchar(b);
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is my alx Functions,and  nested lop project.
