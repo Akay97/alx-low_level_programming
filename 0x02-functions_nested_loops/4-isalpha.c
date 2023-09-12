e035e4ab387055a14e3979e3ba046f2718fdb617
Akala Moses <akoredeolamilekan97@gmail.com>
2023-09-12 11:28:48 -0700

    Write a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function that check for alphabeth character
+ * @c: th checking for this charater
+ * Return: 1 if c is a letter, lowercase or uppercase, otherwise return 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
