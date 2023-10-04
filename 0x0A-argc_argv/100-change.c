1881a983b928885b39ad96a1b5f08033d5bf933b
Akala Moses <akoredeolamilekan97@gmail.com>
2023-10-04 09:32:13 -0700

    argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,49 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - the function name
- * @argc: the argument counter
- * @argv: the argument variable
- * Return: 0 as success
- */
-
 int main(int argc, char *argv[])
 {
-	int cents;
-	int coins[5];
-	int num_coins;
-	int count;
-	int i;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+	int n_coins, cents, coins, n_coins, i;
 
-	cents = atoi(argv[1]);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+    cents = atoi(argv[1]);
 
-	coins[5] = {25, 10, 5, 2, 1};
-	num_coins = sizeof(coins) / sizeof(coins[0]);
-	count = 0;
+    if (cents < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-	for (i = 0; i < num_coins; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			count++;
-		}
-	}
+    coins[] = {25, 10, 5, 2, 1};
+    n_coins = sizeof(coins) / sizeof(coins[0]);
+    n_c = 0;
 
-	printf("%d\n", count);
+    for (i = 0; i < n_coins && cents > 0; i++)
+    {
+        n_c += cents / coins[i];
+        cents %= coins[i];
+    }
 
-	return (0);
+    printf("%d\n", n_c);
+    return 0;
 }
+
+
