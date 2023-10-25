c129780c01b8f47dd29a547e41f1356ed2c9c3e1
Akala Moses <akoredeolamilekan97@gmail.com>
2023-10-25 06:54:47 -0700

    this is my recursion project

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - the function name
+ * @s: the string to be reversed
+ * Return: 0 always success
+ */
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+/**
+ * _sqrt_recursion_sq - the function name
+ * @n: the first integer
+ * @a: the second integer
+ * @b: the third integer
+ * Return: 0 always success
+ */
+
+int _sqrt_recursion_sq(int n, int a, int b)
+{
+	if (a * a == n)
+	{
+		return (a);
+	}
+	else if (a * a > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion_sq(n, a + 1, b));
+	}
+}
+
+/**
+ * _sqrt_recursion - the function name
+ * @n: the number which square root will be returned
+ * Return: 0 always success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion_sq(n, 0, n));
+}
