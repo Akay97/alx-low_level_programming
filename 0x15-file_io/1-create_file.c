3111322f621ddc56490732422162dd557545e403
Akala Moses <akoredeolamilekan97@gmail.com>
2023-11-27 08:07:06 -0800

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * create_file - a funcion that createsa a file
+ * with a name and writes the given text content to it.`
+ * @filename: The name of the file to create.
+ * @text_content: The terminated string to write to the file.
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f_descript;
+	ssize_t bytes_writ;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	f_descript = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (f_descript == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_writ = write(f_descript, text_content, strlen(text_content));
+		if (bytes_writ == -1)
+		{
+			close(f_descript);
+			return (-1);
+		}
+	}
+
+	close(f_descript);
+
+	return (1);
+}
